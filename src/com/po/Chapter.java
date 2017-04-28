86d6168bfce1363d779b8564d4a1dceed345cf02
kosmas <zyx3166@126.com>
2017-04-28 18:02:09 +0800

    555566

---
+++ src/com/po/Chapter.java
@@ -0,0 +1,30 @@
+package com.po;
+
+public class Chapter {
+	
+	private int id;
+	private String name;
+	private int courseId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	
+	
+
+
+}
--- src/com/po/Video.java
+++ src/com/po/Video.java
@@ -3,6 +3,7 @@ package com.po;
 public class Video {
 	private String name;
 	private int id;
+	private int chapterId;
 	
 
 	public String getName() {
@@ -11,11 +12,17 @@ public class Video {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getChapter() {
+	public int getId() {
 		return id;
 	}
-	public void setChapter(int id) {
+	public void setId(int id) {
 		this.id = id;
+	}
+	public int getChapterId() {
+		return chapterId;
+	}
+	public void setChapterId(int chapterId) {
+		this.chapterId = chapterId;
 	}
 	
 
